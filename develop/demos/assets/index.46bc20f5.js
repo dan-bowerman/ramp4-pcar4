69b0c7959e7af06eb1911d21b1462784ff2c2f5d
milespetrov <miles.petrov@ec.gc.ca>
2022-05-26 14:51:11 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@eff89db1a12874c6dec24c10ecef46da9fc4ac95 🚀

---
+++ develop/demos/assets/index.46bc20f5.js
@@ -0,0 +1,1 @@
+import{f as o}from"./fabric.1a1b06a1.js";import{eR as n}from"./main.0c522c3b.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
---
+++ develop/demos/assets/vue.esm-bundler.afa7a02e.js
@@ -0,0 +1,1 @@
+import{f0 as e,fn as r}from"./main.0c522c3b.js";var m=e(r);export{m as r};
