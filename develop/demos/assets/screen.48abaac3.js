77b11c8fde0cc24b8f3632fce4d5c1acda625480
milespetrov <miles.petrov@ec.gc.ca>
2022-05-26 18:45:36 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@723575d4209c134a2ca1586fc181cb50859ed04e 🚀

---
+++ develop/demos/assets/screen.48abaac3.js
@@ -0,0 +1,221 @@
+import{eu as C,et as b,eF as g,ew as d,eG as v,eH as p,eX as S,eJ as w,eP as f,eA as i,eI as m,ex as x,eL as h}from"./main.bfcab78f.js";var $=`<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:gmd="http://www.isotc211.org/2005/gmd"
+                xmlns:gco="http://www.isotc211.org/2005/gco"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:gmdl="http://www.canada.gc.ca/ns/gmdl"
+                xmlns:napec="http://www.ec.gc.ca/data_donnees/standards/schemas/napec"
+                xmlns:gml="http://www.opengis.net/gml"
+                xmlns:geonet="http://www.fao.org/geonetwork"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.ec.gc.ca/data_donnees/standards/schemas/napec/schema.xsd">
+
+  <xsl:param name="catalogue_url" />
+  <xsl:decimal-format NaN=""/>
+
+  <xsl:template match="/">
+
+    <div class="metadata-view">
+
+      <xsl:if test="//gmd:abstract/gco:CharacterString/text() != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Abstract}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:abstract/gco:CharacterString/text()" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Scope}}</h5>
+        <p>{{metadata.xslt.hereBeScope}}</p>
+      </xsl:comment>
+
+      <xsl:if test="//gml:TimePeriod//* != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.timePeriod}}</h5>
+        <p>
+          <xsl:value-of select="//gml:TimePeriod//gml:beginPosition" />
+          <xsl:if test="//gml:TimePeriod//gml:beginPosition/text() != '' and //gml:TimePeriod//gml:endPosition/text() != ''">
+            -
+          </xsl:if>
+          <xsl:value-of select="//gml:TimePeriod//gml:endPosition" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <xsl:if test="//gmd:supplementalInformation/gco:CharacterString/text() != ''">
+          <h5 class="text-xl font-bold mb-3">{{metadata.xslt.supplementalData}}</h5>
+          <p>
+            <xsl:value-of select="//gmd:supplementalInformation/gco:CharacterString/text()" />
+          </p>
+        </xsl:if>
+      </xsl:comment>
+
+      <xsl:if test="//gmd:pointOfContact//gmd:individualName/* != '' 
+              or //gmd:pointOfContact//gmd:organisationName/gco:CharacterString/text() != ''
+              or //gmd:pointOfContact//gmd:positionName/gco:CharacterString/text() != ''
+              or //gmd:pointOfContact//gmd:electronicMailAddress/* != ''
+              or //gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.contactInfo}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:individualName" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:organisationName/gco:CharacterString/text()" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:positionName/gco:CharacterString/text()" />
+        </p>
+        <p>
+          <a href="mailto:{//gmd:pointOfContact//gmd:electronicMailAddress/gco:CharacterString/text()}?Subject={//gmd:identificationInfo//gmd:title/gco:CharacterString/text()}">
+            <xsl:value-of select="//gmd:pointOfContact//gmd:electronicMailAddress" />
+          </a>
+        </p>
+        <p>
+          <xsl:variable name="roleCode" >
+            <xsl:value-of select="concat(substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue,1,1),
+                        substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue, 2))" />
+          </xsl:variable>
+
+          <xsl:choose>
+            <xsl:when test="$roleCode = 'resourceProvider'">{{metadata.xslt.resourceProvider}}</xsl:when>
+            <xsl:when test="$roleCode = 'custodian'">{{metadata.xslt.custodian}}</xsl:when>
+            <xsl:when test="$roleCode = 'owner'">{{metadata.xslt.owner}}</xsl:when>
+            <xsl:when test="$roleCode = 'user'">{{metadata.xslt.user}}</xsl:when>
+            <xsl:when test="$roleCode = 'distributor'">{{metadata.xslt.distributor}}</xsl:when>
+            <xsl:when test="$roleCode = 'originator'">{{metadata.xslt.originator}}</xsl:when>
+            <xsl:when test="$roleCode = 'pointOfContact'">{{metadata.xslt.pointOfContact}}</xsl:when>
+            <xsl:when test="$roleCode = 'principalInvestigator'">{{metadata.xslt.principalInvestigator}}</xsl:when>
+            <xsl:when test="$roleCode = 'processor'">{{metadata.xslt.processor}}</xsl:when>
+            <xsl:when test="$roleCode = 'publisher'">{{metadata.xslt.publisher}}</xsl:when>
+            <xsl:when test="$roleCode = 'author'">{{metadata.xslt.author}}</xsl:when>
+            <xsl:when test="$roleCode = 'collaborator'">{{metadata.xslt.collaborator}}</xsl:when>
+            <xsl:when test="$roleCode = 'editor'">{{metadata.xslt.editor}}</xsl:when>
+            <xsl:when test="$roleCode = 'mediator'">{{metadata.xslt.mediator}}</xsl:when>
+            <xsl:when test="$roleCode = 'rightsHolder'">{{metadata.xslt.rightsHolder}}</xsl:when>
+          </xsl:choose>
+        </p>
+      </xsl:if>
+
+      <xsl:if test="$catalogue_url != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.cataloguePage}}</h5>
+        <p>
+          <a href="{$catalogue_url}"
+             rel="external" target="_blank" class="ui-link">
+            {{metadata.xslt.metadata}}
+          </a>
+        </p>
+      </xsl:if>
+    </div>
+  </xsl:template>
+</xsl:stylesheet>
+`,L=`<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:gmd="http://www.isotc211.org/2005/gmd"
+                xmlns:gco="http://www.isotc211.org/2005/gco"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:gmdl="http://www.canada.gc.ca/ns/gmdl"
+                xmlns:napec="http://www.ec.gc.ca/data_donnees/standards/schemas/napec"
+                xmlns:gml="http://www.opengis.net/gml"
+                xmlns:geonet="http://www.fao.org/geonetwork"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.ec.gc.ca/data_donnees/standards/schemas/napec/schema.xsd">
+
+  <xsl:param name="catalogue_url" />
+  <xsl:decimal-format NaN=""/>
+
+  <xsl:template match="/">
+
+    <div class="metadata-view">
+
+      <xsl:if test="//gmd:abstract//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Abstract}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:abstract//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.Scope}}</h5>
+        <p>{{metadata.xslt.hereBeScope}}</p>
+      </xsl:comment>
+
+      <xsl:if test="//gml:TimePeriod//* != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.timePeriod}}</h5>
+        <p>
+          <xsl:value-of select="//gml:TimePeriod//gml:beginPosition" />
+          <xsl:if test="//gml:TimePeriod//gml:beginPosition/text() != '' and //gml:TimePeriod//gml:endPosition/text() != ''">
+            -
+          </xsl:if>
+          <xsl:value-of select="//gml:TimePeriod//gml:endPosition" />
+        </p>
+      </xsl:if>
+
+      <xsl:comment>
+        <xsl:if test="//gmd:supplementalInformation//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''">
+          <h5 class="text-xl font-bold mb-3">{{metadata.xslt.supplementalData}}</h5>
+          <p>
+            <xsl:value-of select="//gmd:supplementalInformation//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+          </p>
+        </xsl:if>
+      </xsl:comment>
+
+      <xsl:if test="//gmd:pointOfContact//gmd:individualName/* != '' 
+              or //gmd:pointOfContact//gmd:organisationName//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''
+              or //gmd:pointOfContact//gmd:positionName//gmd:LocalisedCharacterString[@locale='#fra']/text() != ''
+              or //gmd:pointOfContact//gmd:electronicMailAddress/* != ''
+              or //gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.contactInfo}}</h5>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:individualName" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:organisationName//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+        </p>
+        <p>
+          <xsl:value-of select="//gmd:pointOfContact//gmd:positionName//gmd:LocalisedCharacterString[@locale='#fra']/text()" />
+        </p>
+        <p>
+          <a href="mailto:{//gmd:pointOfContact//gmd:electronicMailAddress//gmd:LocalisedCharacterString[@locale='#fra']/text()}?Subject={//gmd:identificationInfo//gmd:title//gmd:LocalisedCharacterString[@locale='#fra']/text()}">
+            <xsl:value-of select="//gmd:pointOfContact//gmd:electronicMailAddress" />
+          </a>
+        </p>
+        <p>
+          <xsl:variable name="roleCode" >
+            <xsl:value-of select="concat(substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue,1,1),
+                        substring(//gmd:pointOfContact//gmd:role/gmd:CI_RoleCode/@codeListValue, 2))" />
+          </xsl:variable>
+
+          <xsl:choose>
+            <xsl:when test="$roleCode = 'resourceProvider'">{{metadata.xslt.resourceProvider}}</xsl:when>
+            <xsl:when test="$roleCode = 'custodian'">{{metadata.xslt.custodian}}</xsl:when>
+            <xsl:when test="$roleCode = 'owner'">{{metadata.xslt.owner}}</xsl:when>
+            <xsl:when test="$roleCode = 'user'">{{metadata.xslt.user}}</xsl:when>
+            <xsl:when test="$roleCode = 'distributor'">{{metadata.xslt.distributor}}</xsl:when>
+            <xsl:when test="$roleCode = 'originator'">{{metadata.xslt.originator}}</xsl:when>
+            <xsl:when test="$roleCode = 'pointOfContact'">{{metadata.xslt.pointOfContact}}</xsl:when>
+            <xsl:when test="$roleCode = 'principalInvestigator'">{{metadata.xslt.principalInvestigator}}</xsl:when>
+            <xsl:when test="$roleCode = 'processor'">{{metadata.xslt.processor}}</xsl:when>
+            <xsl:when test="$roleCode = 'publisher'">{{metadata.xslt.publisher}}</xsl:when>
+            <xsl:when test="$roleCode = 'author'">{{metadata.xslt.author}}</xsl:when>
+            <xsl:when test="$roleCode = 'collaborator'">{{metadata.xslt.collaborator}}</xsl:when>
+            <xsl:when test="$roleCode = 'editor'">{{metadata.xslt.editor}}</xsl:when>
+            <xsl:when test="$roleCode = 'mediator'">{{metadata.xslt.mediator}}</xsl:when>
+            <xsl:when test="$roleCode = 'rightsHolder'">{{metadata.xslt.rightsHolder}}</xsl:when>
+          </xsl:choose>
+        </p>
+      </xsl:if>
+
+      <xsl:if test="$catalogue_url != ''">
+        <h5 class="text-xl font-bold mb-3">{{metadata.xslt.cataloguePage}}</h5>
+        <p>
+          <a href="{$catalogue_url}"
+             rel="external" target="_blank" class="ui-link">
+            {{metadata.xslt.metadata}}
+          </a>
+        </p>
+      </xsl:if>
+    </div>
+  </xsl:template>
+</xsl:stylesheet>
+`;const y=b({name:"MetadataScreenV",props:{panel:{type:Object},payload:{type:Object,required:!0}},data(){return{state:{status:"loading",response:null},cache:{}}},mounted(){this.payload.type==="xml"?this.loadFromURL(this.payload.url,[]).then(e=>{this.state.status="success",e!==null&&this.$el.childNodes[1].appendChild(e)}):this.payload.type==="html"&&this.requestContent(this.payload.url).then(e=>{this.state.status="success",this.state.response=e.response})},methods:{loadFromURL(e,n){let s=this.$iApi.language==="en"?$:L;return s=s.replace(/\{\{([\w.]+)\}\}/g,(t,a)=>this.$t(a)),this.cache[e]?Promise.resolve(this.applyXSLT(this.cache[e],s,n)):this.requestContent(e).then(t=>(this.cache[e]=t.response,this.applyXSLT(this.cache[e],s,n)))},applyXSLT(e,n,s){let t=null;if(window.XSLTProcessor){const a=new window.XSLTProcessor,l=new DOMParser,r=l.parseFromString(e,"text/xml"),o=l.parseFromString(n,"text/xml");a.importStylesheet(o),s&&s.forEach(c=>a.setParameter("",c.key,c.value||"")),t=a.transformToFragment(r,document)}else if(window.hasOwnProperty("ActiveXObject")){const a=new window.ActiveXObject("Msxml2.XSLTemplate"),l=new window.ActiveXObject("Msxml2.DOMDocument"),r=new window.ActiveXObject("Msxml2.FreeThreadedDOMDocument");l.loadXML(e),r.loadXML(n),a.stylesheet=r;const o=a.createProcessor();o.input=l,o.transform(),t=document.createRange().createContextualFragment(o.output)}return t},requestContent(e){return new Promise((n,s)=>{const t=new XMLHttpRequest;t.open("GET",e,!0),t.responseType="text",t.onload=()=>{t.status===200?n({status:"success",response:t.response}):n({status:"error",response:"Could not load results from remote service."})},t.onerror=()=>{n({status:"error",response:"Could not load results from remote service."})},t.send()})}}}),O={class:"flex justify-center"},P={key:0,class:"flex flex-col justify-center text-center"},_=i("div",{class:"flex flex-col justify-center"},null,-1),T=["innerHTML"],M=i("div",{class:"flex flex-col justify-center text-center"},[i("img",{src:"https://i.imgur.com/fA5EqV6.png"}),i("span",{class:"text-xl mt-20"},"There was an error retrieving this resource. Please try again.")],-1);function N(e,n,s,t,a,l){const r=g("minimize"),o=g("close"),c=g("panel-screen");return d(),v(c,null,{header:p(()=>[S(w(e.$t("metadata.title"))+": "+w(e.payload.layerName),1)]),controls:p(()=>[f(r,{onClick:n[0]||(n[0]=u=>e.panel.minimize())}),f(o,{onClick:n[1]||(n[1]=u=>e.panel.close())})]),content:p(()=>[i("div",O,[m(" Loading Screen "),e.state.status=="loading"?(d(),x("div",P," Loading... ")):e.payload.type==="xml"&&e.state.status=="success"?(d(),x(h,{key:1},[m(" Found Screen, XML "),_],2112)):e.payload.type==="html"&&e.state.status=="success"?(d(),x(h,{key:2},[m(" Found Screen, HTML "),i("div",{innerHTML:e.state.response,class:"flex flex-col justify-center max-w-full"},null,8,T)],2112)):(d(),x(h,{key:3},[m(" Error Screen "),M],2112))])]),_:1})}var k=C(y,[["render",N],["__file","/home/runner/work/ramp4-pcar4/ramp4-pcar4/src/fixtures/metadata/screen.vue"]]);export{k as default};
--- develop/demos/assets/vue.esm-bundler.e8d15d99.js
+++
@@ -1,1 +0,0 @@
-import{f0 as e,fn as r}from"./main.ecb690b2.js";var m=e(r);export{m as r};
