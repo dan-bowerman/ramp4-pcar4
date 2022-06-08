17476ff0a50511b82faf7f855c250c3188812c8d
Miles Petrov <miles.petrov@ec.gc.ca>
2022-06-08 13:41:28 +0000

    Delete PR demo test-pr

--- test-pr/demos/assets/jsonContext.f2faad6d.js
+++
@@ -1,1 +0,0 @@
-import{O as t,B as e}from"./main.4c7c23d9.js";function a(r){return{origin:"portal-item",url:t(r.itemUrl),portal:r.portal||e.getDefault(),portalItem:r,readResourcePaths:[]}}export{a as o};
--- test-pr/demos/assets/vue.esm-bundler.228680e8.js
+++
@@ -1,1 +0,0 @@
-import{f2 as e,fq as r}from"./main.4c7c23d9.js";var m=e(r);export{m as r};
