06cfd8671c280ec95c70c97a6b5c8182e0e7998b
Miles Petrov <miles.petrov@ec.gc.ca>
2022-06-08 19:33:54 +0000

    Delete PR demo div-height

--- div-height/demos/assets/index.3a3496f0.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./fabric.f1cd6fd2.js";import{eQ as n}from"./main.01d97856.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
--- div-height/demos/assets/vue.esm-bundler.15da32dd.js
+++
@@ -1,1 +0,0 @@
-import{f6 as e,fs as r}from"./main.01d97856.js";var s=e(r);export{s as r};
