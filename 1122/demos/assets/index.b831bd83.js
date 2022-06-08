56035caeeea4471deea6f36216f74b797b69c3c6
roryhofland <roryhofland@users.noreply.github.com>
2022-06-08 19:35:06 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@177e9a69aa2284d282bfa9e6892873c41ab40429 🚀

---
+++ 1122/demos/assets/index.b831bd83.js
@@ -0,0 +1,1 @@
+import{f as o}from"./fabric.f6aa7ea6.js";import{eQ as n}from"./main.c8cfc77c.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
---
+++ 1122/demos/assets/vue.esm-bundler.b2933604.js
@@ -0,0 +1,1 @@
+import{f6 as e,fs as r}from"./main.c8cfc77c.js";var s=e(r);export{s as r};
