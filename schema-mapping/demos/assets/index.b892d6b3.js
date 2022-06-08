845bebfe2be057adc124492b8382a67453d08aa2
mohsin-r <mohsin-r@users.noreply.github.com>
2022-06-08 17:59:34 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@177e9a69aa2284d282bfa9e6892873c41ab40429 🚀

--- schema-mapping/demos/assets/index.b892d6b3.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./fabric.6524ea7f.js";import{eQ as n}from"./main.4f808ac9.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
