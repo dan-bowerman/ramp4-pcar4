ad4e711dd6041b557d5927e72a6f6a0d3ee6601a
james-rae <james.b.rae@gmail.com>
2022-06-13 14:24:06 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@8d892db0774a48db3986c97b930cc123e7f12add 🚀

---
+++ main/demos/assets/LercWorker.b98106ef.js
@@ -0,0 +1,1 @@
+import{hy as s}from"./main.186bf612.js";class t{_decode(e){const r=s(e.buffer,e.options);return Promise.resolve({result:r,transferList:[r.pixelData.buffer]})}}function n(){return new t}export{n as default};
---
+++ main/demos/assets/index.d3abbb58.js
@@ -0,0 +1,1 @@
+import{f as o}from"./fabric.ed87bc2f.js";import{eQ as n}from"./main.186bf612.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
