1715f125e78c872b70e2c329a6e3b1aaf5affed1
Miles Petrov <miles.petrov@ec.gc.ca>
2022-06-14 21:09:24 +0000

    Delete PR demo composition-multi-ramp

--- composition-multi-ramp/demos/assets/LercWorker.1a3267cf.js
+++
@@ -1,1 +0,0 @@
-import{hw as s}from"./multi-ramp.88e660b2.js";class t{_decode(e){const r=s(e.buffer,e.options);return Promise.resolve({result:r,transferList:[r.pixelData.buffer]})}}function n(){return new t}export{n as default};
--- composition-multi-ramp/demos/assets/vue.esm-bundler.01a724b6.js
+++
@@ -1,1 +0,0 @@
-import{f4 as e,fq as r}from"./multi-ramp.88e660b2.js";var m=e(r);export{m as r};
