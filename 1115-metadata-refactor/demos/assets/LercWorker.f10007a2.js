1338b6f81628b8f8db6da824d4c67189b7dcba71
yileifeng <yileifeng@users.noreply.github.com>
2022-06-15 17:23:44 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@202ab55af85c22284b88ec0dcfdb1eda3014ad01 🚀

--- 1115-metadata-refactor/demos/assets/LercWorker.f10007a2.js
+++
@@ -1,1 +0,0 @@
-import{hy as s}from"./main.bf5b0f8f.js";class t{_decode(e){const r=s(e.buffer,e.options);return Promise.resolve({result:r,transferList:[r.pixelData.buffer]})}}function n(){return new t}export{n as default};
--- 1115-metadata-refactor/demos/assets/jsonContext.1fcf5ac2.js
+++
@@ -1,1 +0,0 @@
-import{O as t,B as e}from"./main.bf5b0f8f.js";function a(r){return{origin:"portal-item",url:t(r.itemUrl),portal:r.portal||e.getDefault(),portalItem:r,readResourcePaths:[]}}export{a as o};
