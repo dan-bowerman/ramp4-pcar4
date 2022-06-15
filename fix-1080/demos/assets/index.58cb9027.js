88de97631b164f676b6210b19a3aee9bd1e1b25a
RyanCoulsonCA <RyanCoulsonCA@users.noreply.github.com>
2022-06-15 11:12:28 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@202ab55af85c22284b88ec0dcfdb1eda3014ad01 🚀

---
+++ fix-1080/demos/assets/index.58cb9027.js
@@ -0,0 +1,1 @@
+import{f as o}from"./fabric.3692b479.js";import{eQ as n}from"./main.924a0d32.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
--- fix-1080/demos/assets/vue.esm-bundler.fd67b9dd.js
+++
@@ -1,1 +0,0 @@
-import{f6 as e,fs as r}from"./main.32ef8a5a.js";var s=e(r);export{s as r};
