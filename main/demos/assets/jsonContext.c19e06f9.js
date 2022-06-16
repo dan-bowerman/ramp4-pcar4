39c0cbf841b24a1f54e0c6517ed1661dbbd6a2d5
james-rae <james.b.rae@gmail.com>
2022-06-16 19:57:42 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@c25ad4fcbec607b751da23743d1223a1eb20e0f7 🚀

---
+++ main/demos/assets/jsonContext.c19e06f9.js
@@ -0,0 +1,1 @@
+import{O as t,B as e}from"./main.8fa2b18c.js";function a(r){return{origin:"portal-item",url:t(r.itemUrl),portal:r.portal||e.getDefault(),portalItem:r,readResourcePaths:[]}}export{a as o};
---
+++ main/demos/assets/vue.esm-bundler.b573f284.js
@@ -0,0 +1,1 @@
+import{f7 as e,fs as r}from"./main.8fa2b18c.js";var s=e(r);export{s as r};
