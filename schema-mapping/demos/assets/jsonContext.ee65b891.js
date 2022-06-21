60eb486aa9a03bc59ffe0958874ae697fddeb987
mohsin-r <mohsin-r@users.noreply.github.com>
2022-06-21 16:55:18 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@ff2fb830cca266af541a02903457034567355e5a 🚀

---
+++ schema-mapping/demos/assets/jsonContext.ee65b891.js
@@ -0,0 +1,1 @@
+import{O as t,B as e}from"./main.1c3c0e9b.js";function a(r){return{origin:"portal-item",url:t(r.itemUrl),portal:r.portal||e.getDefault(),portalItem:r,readResourcePaths:[]}}export{a as o};
--- schema-mapping/demos/assets/vue.esm-bundler.018cdd39.js
+++
@@ -1,1 +0,0 @@
-import{f7 as e,fs as r}from"./main.8e56079f.js";var s=e(r);export{s as r};
