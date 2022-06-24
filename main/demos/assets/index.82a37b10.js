91de1e0e4d29345f10680503509126c2e1fd75e0
james-rae <james.b.rae@gmail.com>
2022-06-24 19:34:03 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@6d87a02c5518a2ce49e11f8766355e17c7d0d42c 🚀

---
+++ main/demos/assets/index.82a37b10.js
@@ -0,0 +1,1 @@
+import{f as o}from"./fabric.0ac8b857.js";import{eR as n}from"./main.1dea2a78.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
--- main/demos/assets/vue.esm-bundler.51decfb8.js
+++
@@ -1,1 +0,0 @@
-import{f7 as e,fs as r}from"./main.02caa7b6.js";var s=e(r);export{s as r};
