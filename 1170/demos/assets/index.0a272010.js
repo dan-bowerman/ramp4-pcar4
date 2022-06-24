5b9c783b98ffaee661cb027a17ca3a47194a9470
Miles Petrov <miles.petrov@ec.gc.ca>
2022-06-24 19:36:53 +0000

    Delete PR demo 1170

--- 1170/demos/assets/index.0a272010.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./fabric.dbbf3a18.js";import{eR as n}from"./main.0395d05a.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
--- 1170/demos/assets/vue.esm-bundler.505855bd.js
+++
@@ -1,1 +0,0 @@
-import{f7 as e,fs as r}from"./main.0395d05a.js";var s=e(r);export{s as r};
