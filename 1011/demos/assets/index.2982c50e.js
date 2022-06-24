9df24c21f5a7500b91a86facef73cef62d16768d
sharvenp <sharvenp@users.noreply.github.com>
2022-06-24 15:14:45 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@393dc7e26b878cc49947933e2c72e150e5dbd59a 🚀

--- 1011/demos/assets/index.2982c50e.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./fabric.49f3673c.js";import{eR as n}from"./main.e0908dae.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
--- 1011/demos/assets/vue.esm-bundler.f60657d1.js
+++
@@ -1,1 +0,0 @@
-import{f7 as e,fs as r}from"./main.e0908dae.js";var s=e(r);export{s as r};
