473e9bb0b109c683accd58493bbaaa58f69e84d5
spencerwahl <spencerwahl@users.noreply.github.com>
2022-06-28 13:40:38 +0000

    Deploying to gh-pages from @ ramp4-pcar4/ramp4-pcar4@41db698848b82cf822e49cd64fbed80e1aa2f18f 🚀

---
+++ appbar-schema-fix/demos/assets/index.c812c555.js
@@ -0,0 +1,1 @@
+import{f as o}from"./fabric.ca60a39e.js";import{eR as n}from"./main.855c0a69.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
