939454c9b7220a860fa63f5b03c0d2896b09ac3d
Miles Petrov <miles.petrov@ec.gc.ca>
2022-06-28 15:18:01 +0000

    Delete PR demo 1171

--- 1171/demos/assets/index.4b9e432d.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./fabric.8aae8bfa.js";import{eR as n}from"./main.1458f4c9.js";class m extends n{get config(){return this.$iApi.fixture.get("export").config?.map}async make(t){const a=await this.$iApi.geo.map.takeScreenshot({quality:1,format:"png"}),e=new Image;e.src=a.dataUrl;const i=await new Promise(r=>e.onload=()=>r(e));return new o.fabric.Image(i,t)}}export{m as default};
